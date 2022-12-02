860e4e7a19d06d2c9c9076fa5deccc5107e7a7ba
Thomas Popp <dev@thomas-popp.de>
2022-12-02 21:38:35 +0100

    formatting

--- src/test/kotlin/day1/Day1.kt
+++ src/test/kotlin/day1/Day1.kt
@@ -7,13 +7,13 @@ import kotlin.test.assertEquals
 internal class Day1 {
 
     @Test
-    fun sampleSilver(){
+    fun sampleSilver() {
         val elves = create_elves(read_puzzle_input("sample_silver"))
         assertEquals(24000, elves.map { it.sum() }.max())
     }
 
     @Test
-    fun sampleGold(){
+    fun sampleGold() {
         val elves = create_elves(read_puzzle_input("sample_silver"))
         assertEquals(45000, elves.map { it.sum() }.sortedDescending().take(3).sum())
     }
@@ -25,7 +25,7 @@ internal class Day1 {
     }
 
     @Test
-    fun gold(){
+    fun gold() {
         val elves = create_elves(read_puzzle_input("input_silver"))
         assertEquals(208437, elves.map { it.sum() }.sortedDescending().take(3).sum())
     }
@@ -37,11 +37,11 @@ fun Elv.sum(): Int {
     return snacks.sum()
 }
 
-fun create_elves( raw : String): List<Elv> {
-    return raw.split("\n\n").map { Elv(it.split("\n").map { it.toInt() } )}
+fun create_elves(raw: String): List<Elv> {
+    return raw.split("\n\n").map { Elv(it.split("\n").map { it.toInt() }) }
 }
 
-fun read_puzzle_input( filename : String): String {
-        return File("src/test/kotlin/day1/$filename")
-            .readText(Charsets.UTF_8)
+fun read_puzzle_input(filename: String): String {
+    return File("src/test/kotlin/day1/$filename")
+        .readText(Charsets.UTF_8)
 }
--- src/test/kotlin/day2/Day2.kt
+++ src/test/kotlin/day2/Day2.kt
@@ -5,33 +5,33 @@ import java.lang.RuntimeException
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-internal class Day2{
+internal class Day2 {
 
     @Test
-    fun sampleSilver(){
+    fun sampleSilver() {
         val input = read_puzzle_input("sample_silver")
-        val total = rounds(input).map { it.score() }.sum()
+        val total = rounds(input).sumOf { it.score() }
         assertEquals(total, 15)
     }
 
     @Test
-    fun sampleGold(){
+    fun sampleGold() {
         val input = read_puzzle_input("sample_silver")
-        val total = rounds(input).map { it.cheat().score() }.sum()
+        val total = rounds(input).sumOf { it.cheat().score() }
         assertEquals(total, 12)
     }
 
     @Test
     fun silver() {
         val input = read_puzzle_input("input_silver")
-        val total = rounds(input).map { it.score() }.sum()
+        val total = rounds(input).sumOf { it.score() }
         assertEquals(total, 14297)
     }
 
     @Test
-    fun gold(){
+    fun gold() {
         val input = read_puzzle_input("input_silver")
-        val total = rounds(input).map { it.cheat().score() }.sum()
+        val total = rounds(input).sumOf { it.cheat().score() }
         assertEquals(total, 10498)
     }
 }
@@ -41,15 +41,14 @@ enum class Shape(val score: Int, val defeatedByOrdinal: Int, val beatsOrdinal: I
     PAPER(2, 2, 0),
     SCISSORS(3, 0, 1);
 
-    fun play(shape : Shape) : Int{
-        if(shape == this){
-            return 3;
-        }
-        else if (shape == values()[defeatedByOrdinal]) {
+    fun play(shape: Shape): Int {
+        if (shape == this) {
+            return 3
+        } else if (shape == values()[defeatedByOrdinal]) {
             return 0
         }
-        return 6;
-    };
+        return 6
+    }
 
     companion object {
         fun fromChar(c: Char): Shape {
@@ -70,21 +69,21 @@ enum class Shape(val score: Int, val defeatedByOrdinal: Int, val beatsOrdinal: I
 
 data class Round(val victimMove: Shape, var playerMove: Shape) {
     companion object {
-        fun fromList(list: List<String>) : Round {
+        fun fromList(list: List<String>): Round {
             return Round(Shape.fromChar(list.first().first()), Shape.fromChar(list.last().first()))
         }
     }
 }
 
-fun Round.cheat() : Round{
-    this.playerMove = when(this.playerMove){
+fun Round.cheat(): Round {
+    this.playerMove = when (this.playerMove) {
         // NEED TO LOSE
         Shape.ROCK -> {
             Shape.values()[this.victimMove.beatsOrdinal]
         }
         // DRAW
         Shape.PAPER -> {
-            this.victimMove;
+            this.victimMove
         }
         // NEED TO WIN
         Shape.SCISSORS -> {
@@ -95,15 +94,15 @@ fun Round.cheat() : Round{
 }
 
 
-fun Round.score() : Int {
+fun Round.score(): Int {
     return playerMove.play(victimMove) + this.playerMove.score
 }
 
-fun rounds( raw : String): List<Round> {
-    return raw.split("\n").map { Round.fromList(it.split(" "))}
+fun rounds(raw: String): List<Round> {
+    return raw.split("\n").map { Round.fromList(it.split(" ")) }
 }
 
-fun read_puzzle_input( filename : String): String {
+fun read_puzzle_input(filename: String): String {
     return File("src/test/kotlin/day2/$filename")
         .readText(Charsets.UTF_8)
 }
