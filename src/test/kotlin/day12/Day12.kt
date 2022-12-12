0bdea32523b235210e0c38560f0e78e1c79a08f9
Thomas Popp <dev@thomas-popp.de>
2022-12-12 16:52:07 +0100

    refactor(day12)

--- src/test/kotlin/day12/Day12.kt
+++ src/test/kotlin/day12/Day12.kt
@@ -15,7 +15,7 @@ class Day12 {
         val (network, start, end) = read_puzzle_input("sample")
         val graph = simpleDirectedGraph(network)
 
-        assertEquals(31, DijkstraShortestPath.findPathBetween(graph, start.toString(), end.toString()).length)
+        assertEquals(31, DijkstraShortestPath.findPathBetween(graph, start, end).length)
     }
 
 
@@ -27,7 +27,7 @@ class Day12 {
 
         assertEquals(
             29,
-            startPoints.mapNotNull { DijkstraShortestPath.findPathBetween(graph, it.toString(), end.toString()) }
+            startPoints.mapNotNull { DijkstraShortestPath.findPathBetween(graph, it, end) }
                 .minOf { it.length })
     }
 
@@ -36,7 +36,7 @@ class Day12 {
         val (network, start, end) = read_puzzle_input("input")
         val graph = simpleDirectedGraph(network)
 
-        assertEquals(391, DijkstraShortestPath.findPathBetween(graph, start.toString(), end.toString()).length)
+        assertEquals(391, DijkstraShortestPath.findPathBetween(graph, start, end).length)
     }
 
     @Test
@@ -47,26 +47,26 @@ class Day12 {
 
         assertEquals(
             386,
-            start.mapNotNull { DijkstraShortestPath.findPathBetween(graph, it.toString(), end.toString()) }
+            start.mapNotNull { DijkstraShortestPath.findPathBetween(graph, it, end) }
                 .minOf { it.length })
     }
 }
 
 var lookup = "SabcdefghijklmnopqrstuvwxyzE"
 
-data class Edge(val source: String, val target: String, val weight: Int)
+data class Edge(val source: Point, val target: Point, val weight: Int)
 
-fun simpleDirectedGraph(network: Map<Point, Int>): SimpleDirectedGraph<String, Edge> {
-    val graph = SimpleDirectedGraph<String, Edge>(Edge::class.java)
+fun simpleDirectedGraph(network: Map<Point, Int>): SimpleDirectedGraph<Point, Edge> {
+    val graph = SimpleDirectedGraph<Point, Edge>(Edge::class.java)
 
     for (point in network.keys) {
-        graph.addVertex(point.toString())
+        graph.addVertex(point)
     }
 
     for (entry in network) {
         entry.key.directNeighbors().filter { network.containsKey(it) }.forEach {
             if (network[it]!! <= entry.value.plus(1)) {
-                val edge = Edge(entry.key.toString(), it.toString(), 1)
+                val edge = Edge(entry.key, it, 1)
                 graph.addEdge(edge.source, edge.target, edge)
             }
         }
@@ -77,23 +77,24 @@ fun simpleDirectedGraph(network: Map<Point, Int>): SimpleDirectedGraph<String, E
 
 fun read_puzzle_input(filename: String): Triple<Map<Point, Int>, Point, Point> {
     val database = mutableMapOf<Point, Int>()
-    var start = Point(0,0)
-    var end = Point(0,0)
+    var start = Point(0, 0)
+    var end = Point(0, 0)
 
     File("src/test/kotlin/day12/$filename")
         .readLines()
         .withIndex()
         .map { (row, line) ->
             line.withIndex()
-                .forEach() { (column, level) ->
+                .forEach { (column, level) ->
                     database[Point(column, row)] = lookup.indexOf(level)
 
-                    if(level == 'S'){
+                    if (level == 'S') {
                         start = Point(column, row)
-                    } else if(level == 'E'){
+                    } else if (level == 'E') {
                         end = Point(column, row)
                     }
                 }
         }
 
     return Triple(database.toMap(), start, end)
+}
